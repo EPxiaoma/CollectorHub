2b115a99d61a7b3e0252502bc558150071730470
EPxiaoma <599902085@qq.com>
2026-05-07 16:44:41 +0800

    短信登录模块（设置双拦截器，解决状态登录刷新问题）

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -17,15 +18,19 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry){
 
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
                 .excludePathPatterns(
-                        "/shop/**",
-                        "/shop-type/**",
-                        "/voucher/**",
-                        "blog/hot",
                         "/user/code",
                         "/user/login",
-                        "/upload/**"
-                );
+                        "/blog/hot",
+                        "/upload/**",
+                        "/shop-type/**",
+                        "/voucher/**",
+                        "/shop/**"
+                ).order(1);
+
+        // token 刷新拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
     }
 }
